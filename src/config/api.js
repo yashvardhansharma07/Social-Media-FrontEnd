69c2afa066d3ede961e05f969a69a29dcb4a71b6
Yashvardhan Sharma <148075055+yashvardhansharma07@users.noreply.github.com>
2024-09-27 19:36:28 +0530

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-export const API_BASE_URL="http://localhost:5454";
-//export const API_BASE_URL="https://connectingmysphere-production.up.railway.app"
+//export const API_BASE_URL="http://localhost:5454";
+export const API_BASE_URL="https://connectingmysphere-production.up.railway.app"
 
 
 export const api=axios.create({
